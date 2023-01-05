f20a615e9861637c77e9e96dd03500e5adb5d1da
Scott Varieur <savarieur2022@gmail.com>
2023-01-05 15:09:38 -0800

    aaaa

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CategoriesMenu from "./components/categories-menu/categories-menu.component";
+
+const App = () => {
+  return (
+    <>
+      <CategoriesMenu />
+    </>
+  );
+};
+
+export default App;
